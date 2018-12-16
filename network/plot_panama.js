ae9991810dfb9dc8ae0fd5dfb10a9a51d7ee8319
Ruijia <w.ruijia@gmail.com>
2018-12-16 18:32:06 +0100

    fixed network

--- network/plot_panama.js
+++ network/plot_panama.js
@@ -17,8 +17,8 @@ function selectableForceDirectedGraph() {
     //.each(function() { this.focus(); })
     .append("svg")
     .attr("width", width)
-    .attr("height", height);
-
+    .attr("height", height);	
+	
     var zoomer = d3.behavior.zoom().
         scaleExtent([0.1,10]).
         x(xScale).
@@ -27,16 +27,16 @@ function selectableForceDirectedGraph() {
         on("zoom", redraw);
 
     function zoomstart() {
-        node.each(function(d) {
+        /*node.each(function(d) {
             d.selected = false;
             d.previouslySelected = false;
         });
-        node.classed("selected", false);
+        node.classed("selected", false);*/
     }
 
     function redraw() {
-        vis.attr("transform",
-                 "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+        /*vis.attr("transform",
+                 "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");*/
     }
 	
     var brusher = d3.svg.brush()
